bbfb1cb4759090687cd3aa65f9aa8ea370632679
Shubham Jindal <shubhamjindal1234@gmail.com>
2017-04-24 23:55:43 +0530

    P1

---
+++ Program1.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#include<limits.h>
+int main() {
+    printf("Range of short int is from : %d to %d\n",SHRT_MIN,SHRT_MAX);
+    printf("Range of int is from : %d to %d",INT_MIN,INT_MAX);
+}
