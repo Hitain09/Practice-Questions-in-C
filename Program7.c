c98c19819d663eec9d9408da97a848b0917753b6
Shubham Jindal <shubhamjindal1234@gmail.com>
2017-04-28 22:56:10 +0530

    P7

---
+++ Program7.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    float c,d;
+    a = 17;
+    b = a / 2;
+    printf("%d\n",b);
+    printf("%3d\n",b);
+    printf("%03d\n",b);
+    c = 17.356;
+    d = c / 3;
+    printf("%3.2f\n",d);
+}
